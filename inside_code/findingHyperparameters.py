ac9fd90f2dfc2bf80d11cd9209efc6b6da78f9e0
puruvian <hellojunior2021@gmail.com>
2025-05-03 13:31:30 -0700

    found best hyperparams

---
+++ inside_code/findingHyperparameters.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("../lstm/hyperparam_results.csv")
+
+df["sum_accuracy"] = df["training_loss"] + df["held_out_loss"]
+
+tolerance = 0.2  # tolerance for overfitting (i.e. difference between training and held out loss), change as appropriate
+result_count = 10  # how many results you want from the minimum of held_out_loss
+
+filtered = df[(abs(df["training_loss"] - df["held_out_loss"]) < tolerance)]
+three_smallest = filtered.nsmallest(result_count, "held_out_loss")[:10]
+print(three_smallest.iloc[0], three_smallest.iloc[1], three_smallest.iloc[2])  # print first three
+three_smallest.to_csv("../lstm/best_hyperparams.csv")
+
+# plt.scatter(x=df["training_loss"], y=df["held_out_loss"])
+# plt.show()
